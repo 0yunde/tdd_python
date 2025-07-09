429a2e520a7fe66a8892a8cf5eb5e78ae8f5e03d
0yunde <alejandro.yovaniniz@gmail.com>
2025-07-08 21:44:07 -0400

    improve syntax

--- app/reservas.py
+++ app/reservas.py
@@ -1,5 +1,5 @@
 def verificar_disponibilidad(reservas, nueva):
-    for r in reservas:
-        if r["sala"] == nueva["sala"] and r["hora"] == nueva["hora"]:
+    for reser in reservas:
+        if reser["sala"] == nueva["sala"] and r["hora"] == nueva["hora"]:
             return False
     return True
